58e6976d37cebb49231d6769a6131b0ea1bb6e8d
Thomas Lo <thomasjinlo@gmail.com>
2022-07-16 05:58:59 -0700

    add board methods

--- tic-tac-toe/tests/models/board_test.py
+++ tic-tac-toe/tests/models/board_test.py
@@ -3,7 +3,80 @@ from tic_tac_toe.models.board import Board
 
 def test_add_piece():
     board = Board()
-    board.add_piece((0, 0), Board.PIECES.X)
-    assert board.state == [[Board.PIECES.X, None, None],
+    board.add_piece((0, 0), Board.Piece.X)
+    assert board.state == [[Board.Piece.X, None, None],
                            [None, None, None],
                            [None, None, None]]
+
+
+def test_position_is_valid():
+    board = Board()
+    assert board.is_position_valid((0, 0))
+    assert board.is_position_valid((0, 1))
+    assert board.is_position_valid((0, 2))
+    assert board.is_position_valid((1, 0))
+    assert board.is_position_valid((1, 1))
+    assert board.is_position_valid((1, 2))
+    assert board.is_position_valid((2, 0))
+    assert board.is_position_valid((2, 1))
+    assert board.is_position_valid((2, 2))
+
+
+def test_position_is_not_valid():
+    board = Board()
+    assert board.is_position_valid((-1, 0)) is False
+    assert board.is_position_valid((3, 0)) is False
+    assert board.is_position_valid((0, -1)) is False
+    assert board.is_position_valid((0, 3)) is False
+    assert board.is_position_valid((-1, -1)) is False
+    assert board.is_position_valid((-1, 3)) is False
+    assert board.is_position_valid((3, 3)) is False
+    assert board.is_position_valid((3, -1)) is False
+
+
+def test_position_is_empty():
+    board = Board()
+    assert board.is_position_empty((0, 0))
+
+
+def test_position_is_not_empty():
+    board = Board()
+    board.add_piece((0, 0), Board.Piece.X)
+    assert board.is_position_empty((0, 0)) is False
+
+
+def test_has_no_winning_state():
+    board = Board()
+    board.add_piece((0, 0), Board.Piece.X)
+    board.add_piece((1, 1), Board.Piece.X)
+    board.add_piece((2, 2), Board.Piece.O)
+    assert board.has_winning_state() is False
+
+
+def test_has_winning_state_vertically():
+    board = Board()
+    board.add_piece((0, 0), Board.Piece.O)
+    board.add_piece((1, 0), Board.Piece.O)
+    board.add_piece((2, 0), Board.Piece.O)
+    assert board.has_winning_state()
+
+
+def test_has_winning_state_horizontally():
+    board = Board()
+    board.add_piece((0, 0), Board.Piece.O)
+    board.add_piece((0, 1), Board.Piece.O)
+    board.add_piece((0, 2), Board.Piece.O)
+    assert board.has_winning_state()
+
+
+def test_has_winning_state_diagonally():
+    board = Board()
+    board.add_piece((0, 0), Board.Piece.O)
+    board.add_piece((1, 1), Board.Piece.O)
+    board.add_piece((2, 2), Board.Piece.O)
+    assert board.has_winning_state()
+    board = Board()
+    board.add_piece((0, 2), Board.Piece.O)
+    board.add_piece((1, 1), Board.Piece.O)
+    board.add_piece((2, 0), Board.Piece.O)
+    assert board.has_winning_state()
--- tic-tac-toe/tic_tac_toe/models/board.py
+++ tic-tac-toe/tic_tac_toe/models/board.py
@@ -1,7 +1,79 @@
+import pickle
+
+from enum import Enum
+from typing import Any
+
+
 class Board:
+    class Piece(Enum):
+        O = 0
+        X = 1
 
     def __init__(self):
-        pass
+        initial_state = [[None, None, None],
+                         [None, None, None],
+                         [None, None, None]]
+        self.states = [pickle.dumps(initial_state)]
 
-    def add_piece(self):
-        pass
+    @property
+    def state(self):
+        return pickle.loads(self.states[-1])
+
+    def add_piece(self, position: tuple[int, int], piece: Piece) -> None:
+        x, y = position
+        new_state = self.state
+        new_state[x][y] = piece
+
+        self._update_state(new_state)
+
+    def is_position_valid(self, position: tuple[int, int]) -> bool:
+        x, y = position
+
+        return x in range(3) and y in range(3)
+
+    def is_position_empty(self, position: tuple[int, int]) -> bool:
+        x, y = position
+
+        return self.state[x][y] is None
+
+    def has_winning_state(self):
+        return (self._has_winning_vertical_state()
+                or self._has_winning_horizontal_state()
+                or self._has_winning_diagonal_state())
+
+    def _has_winning_vertical_state(self):
+        current_state = self.state
+
+        for col in range(3):
+            if (current_state[0][col]
+                    == current_state[1][col]
+                    == current_state[2][col]):
+                return True
+
+        return False
+
+    def _has_winning_horizontal_state(self):
+        current_state = self.state
+
+        for row in range(3):
+            if (current_state[row][0]
+                    == current_state[row][1]
+                    == current_state[row][2]):
+                return True
+
+        return False
+
+    def _has_winning_diagonal_state(self):
+        current_state = self.state
+
+        return ((current_state[0][0]
+                 == current_state[1][1]
+                 == current_state[2][2])
+                or
+                (current_state[0][2]
+                 == current_state[1][1]
+                 == current_state[2][0]))
+
+    def _update_state(self, state: Any) -> None:
+        serialized_state = pickle.dumps(state)
+        self.states.append(serialized_state)
